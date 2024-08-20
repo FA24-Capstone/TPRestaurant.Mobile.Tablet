4042f2f10a571e0132da76d2a32a6ca9b37a9ce8
phuongvtb <bichphuong13042002@gmail.com>
2024-08-21 03:59:01 +0700

    Combo: Custom combo

--- app/types/combo_type.ts
+++ app/types/combo_type.ts
@@ -9,6 +9,7 @@ export interface Combo {
   category: ComboCategory;
   startDate: string;
   endDate: string;
+  totalOptionSets?: number; // Assuming this is an integer
 }
 
 export interface ComboCategory {
@@ -17,8 +18,70 @@ export interface ComboCategory {
   vietnameseName: string | null;
 }
 
+export interface DishCombo {
+  dishCombo: DishComboDetail[];
+  comboOptionSet: ComboOptionSet;
+}
+
+export interface DishComboDetail {
+  dishComboId: string;
+  quantity: number;
+  dishSizeDetailId: string;
+  dishSizeDetail: DishSizeDetail;
+}
+
+export interface ComboOptionSet {
+  comboOptionSetId: string;
+  optionSetNumber: number;
+  numOfChoice: number;
+  dishItemTypeId: number;
+  dishItemType: any; // Specify further if possible
+  comboId: string;
+  combo: Combo | null;
+}
+
+export interface Dish {
+  dishId: string;
+  name: string;
+  description: string;
+  image: string;
+  dishItemTypeId: number;
+  dishItemType: any;
+  isAvailable: boolean;
+}
+
+export interface DishSizeDetail {
+  dishSizeDetailId: string;
+  isAvailable: boolean;
+  price: number;
+  discount: number;
+  dishId: string;
+  dish: Dish;
+  dishSizeId: number;
+  dishSize: any; // Specify further if possible
+}
+
+export interface DishDetail {
+  dishId: string;
+  name: string;
+  description: string;
+  image: string;
+  dishItemTypeId: number;
+  dishItemType: any; // Replace 'any' with actual type if available
+  isAvailable: boolean;
+}
+
 export interface CombosApiResponse {
   result: {
     items: Combo[];
   };
+}
+
+export interface ComboApiResponseData {
+  combo: Combo;
+  dishCombo: DishCombo[];
+  imgs: string[];
+}
+export interface ComboApiResponse {
+  result: ComboApiResponseData;
 }
